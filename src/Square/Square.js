799ad50172149dbc67f9caca205d6a9ac45c56f5
phakphum ratiwanchai <warmonster2005@hotmail.com>
2020-03-15 01:59:35 +0700

    อัพเดท
    อัพเดท

---
+++ src/Square/Square.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Square.css';
+
+const square = (props) => {
+    return (
+        <button className="square" onClick={props.onClick}>
+            {props.value}
+        </button>
+    );
+}
+
+export default square;
